4b95201441910d25fc6e3dd91ab4943cffbdb736
Brian Auton <brianauton@gmail.com>
2014-03-03 17:14:17 -0500

    Pull out requirement string rendering into RequirementView

--- lib/rspec/contracts/requirement.rb
+++ lib/rspec/contracts/requirement.rb
@@ -1,10 +1,20 @@
+require "rspec/contracts/requirement_view"
+
 module RSpec
   module Contracts
     class Requirement
-      def self.create(contract_name, method_name, options = {})
-        arg_string = options[:arguments] ? "()" : ""
-        return_string = options[:return_value] ? "and return #{options[:return_value].inspect}" : ""
-        puts "Interface '#{contract_name}' must respond to '#{method_name}#{arg_string}' #{return_string}"
+      attr_reader :contract_name, :method_name, :arguments, :return_value
+
+      def initialize(contract_name, method_name, options = {})
+        @contract_name = contract_name
+        @method_name = method_name
+        @arguments = options[:arguments]
+        @return_value = options[:return_value]
+      end
+
+      def self.create(*args)
+        requirement = new(*args)
+        puts RequirementView.new(requirement).render
       end
     end
   end
---
+++ lib/rspec/contracts/requirement_view.rb
@@ -0,0 +1,15 @@
+module RSpec
+  module Contracts
+    class RequirementView
+      def initialize(requirement)
+        @requirement = requirement
+      end
+
+      def render
+        arg_string = @requirement.arguments ? "()" : ""
+        return_string = @requirement.return_value ? "and return #{@requirement.return_value.inspect}" : ""
+        "Interface '#{@requirement.contract_name}' must respond to '#{@requirement.method_name}#{arg_string}' #{return_string}"
+      end
+    end
+  end
+end
