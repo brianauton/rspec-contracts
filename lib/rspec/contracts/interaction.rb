1875d4efc8a7fdf9956f8c7d640916325bdd61e8
Brian Auton <brianauton@gmail.com>
2014-03-05 19:29:12 -0500

    Add specification checking to interface comparison logic

--- lib/rspec/contracts/interaction.rb
+++ lib/rspec/contracts/interaction.rb
@@ -1,18 +1,24 @@
 module RSpec
   module Contracts
     class Interaction
-      attr_reader :interface_name, :method_name, :arguments, :return_value
+      attr_reader :interface_name, :method_name, :arguments, :return_value, :specifications
 
       def initialize(interface_name, method_name, options = {})
         @interface_name = interface_name
         @method_name = method_name
         @arguments = options[:arguments] || []
         @return_value = options[:return_value]
+        @specifications = {}
       end
 
       def fully_described_by?(interaction)
         [:interface_name, :method_name, :arguments, :return_value].select do |attribute|
           return false if interaction.send(attribute) != send(attribute)
+        end
+        @specifications.each do |name, specification|
+          unless specification.fully_described_by? interaction.specifications[name]
+            return false
+          end
         end
         true
       end
