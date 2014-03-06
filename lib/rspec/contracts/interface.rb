321a73d2fec58d1650883481ec17505e3be353c5
Brian Auton <brianauton@gmail.com>
2014-03-06 07:45:54 -0500

    Simplify collection management in Interface

--- lib/rspec/contracts/interface.rb
+++ lib/rspec/contracts/interface.rb
@@ -11,27 +11,19 @@ module RSpec
       end
 
       def add_requirement(method_name, options = {})
-        Interface.add_requirement Interaction.new(name, method_name, options)
+        Interface.requirements.add Interaction.new(name, method_name, options)
       end
 
       def add_implementation(method_name, options = {})
-        Interface.add_implementation Interaction.new(name, method_name, options)
+        Interface.implementations.add Interaction.new(name, method_name, options)
       end
 
       def self.implementations
         @implementations ||= InteractionGroup.new
       end
 
-      def self.add_implementation(interaction)
-        implementations.add interaction
-      end
-
       def self.requirements
         @requrements ||= InteractionGroup.new
-      end
-
-      def self.add_requirement(interaction)
-        requirements.add interaction
       end
     end
   end
